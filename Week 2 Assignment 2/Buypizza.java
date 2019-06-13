c08325f04126216b72479977db48a3f320bd91d1
aju <ajujohn8@gmail.com>
2019-06-13 13:37:11 +0530

    Week 2 Assignment 2

--- Week 2 Assignment 2/Buypizza.java
+++ Week 2 Assignment 2/Buypizza.java
@@ -5,16 +5,16 @@ public class Buypizza{
         String crust = crustPizza;
         
        if(crust.equalsIgnoreCase("ThinCrust")){
-            return price = 50;
+            return price = price + 50;
         }
         else if(crustPizza.equalsIgnoreCase("CheeseBurst")){
-            return price = 65;
+            return price = price + 65;
         }
         else if(crustPizza.equalsIgnoreCase("ClassicPan")){
-            return price = 80;
+            return price = price + 80;
         }
         else{
-            return 0;
+            return price + 0;
         }
         
     }
@@ -23,23 +23,23 @@ public class Buypizza{
 
 
         if(toppingsPizza.equalsIgnoreCase("Corn")){
-            return price = 105;
+            return price = price + 105;
         }
         else if(toppingsPizza.equalsIgnoreCase("Chicken")){
-            return price = 200;
+            return price = price + 200;
         }
         else if(toppingsPizza.equalsIgnoreCase("Paneer")){
-            return price = 135;
+            return price = price + 135;
         }
         else if(toppingsPizza.equalsIgnoreCase("Pineapple")){
-            return price = 125;
+            return price = price + 125;
         }
         else if(toppingsPizza.equalsIgnoreCase("Pepperoni")){
-            return price = 175;
+            return price = price + 175;
         }
 
         else{
-            return price = 0;
+            return price = price + 0;
         }
 
 
@@ -47,25 +47,24 @@ public class Buypizza{
 
     public static int sidesInPizza( String sidesPizza , int price ){
         if(sidesPizza.equalsIgnoreCase("Coke")){
-            return price = 45;
+            return price = price + 45;
         }
         else if(sidesPizza.equalsIgnoreCase("Fries")){
-            return price = 85;
+            return price = price + 85;
         }
         else if(sidesPizza.equalsIgnoreCase("Wedges")){
-            return price = 110;
+            return price = price + 110;
         }
         else if(sidesPizza.equalsIgnoreCase("Nachos")){
-            return price = 150;
+            return price = price + 150;
         }
         else{
-            return 0;
+            return price + 0;
         }
     }
 
     public static int discountInPizza( int discount, int pricepizza ){
         System.out.println("  ");
-        System.out.println("Before Discount price: Rs " +pricepizza);
         if(discount==20){
             return ((pricepizza*80)/100);
          }
@@ -86,69 +85,162 @@ public class Buypizza{
         System.out.println("  ");
         System.out.println("Welcome to Pizza delivery center");
         System.out.println("  ");
-
-        System.out.println("Please enter type of Pizza Crust from the list(do not use space): ");
-        System.out.println("  ");
-
-        System.out.println("1. ThinCrust( Rs 50)");
-        System.out.println("2. CheeseBurst( Rs 65)");
-        System.out.println("3. ClassicPan( Rs 80");
-        System.out.println("  ");
-        System.out.print("Your choice: ");
-
         int priceOfCrust = 0;
         int priceOftoppings = 0;
         int priceOfsides = 0;
         int finalPrizeOfPizza = 0;
-
+        int ans = 0;
+        int count = 0;
+        System.out.print("how many pizza do you want? : ");
         Scanner input = new Scanner(System.in);
+
+        ans = input.nextInt();
+        System.out.println("  ");
+        System.out.println("  ");
+
+        do{
+            
+        
+        System.out.println("Please enter type of Pizza Crust from the list(do not use space): ");
+        System.out.println("  ");
+
+        System.out.println("1. ThinCrust   ( Rs 50)");
+        System.out.println("2. CheeseBurst ( Rs 65)");
+        System.out.println("3. ClassicPan  ( Rs 80");
+        System.out.println("  ");
+        System.out.print("Your choice: ");
+
+        
         String crustRequired = input.next();
+        if( crustRequired.equalsIgnoreCase("ThinCrust") || crustRequired.equalsIgnoreCase("CheeseBurst") || crustRequired.equalsIgnoreCase("ClassicPan")){
         priceOfCrust = crustInPizza( crustRequired , priceOfCrust );
-       
+        }//checking for same menu input
+
+        
+        else if( !crustRequired.equalsIgnoreCase("ThinCrust") || !crustRequired.equalsIgnoreCase("CheeseBurst") || !crustRequired.equalsIgnoreCase("ClassicPan")){
+
+            System.out.print("Your input is not matching with our menu. Please try one more time: ");
+            crustRequired = input.next();
+            {
+            if( crustRequired.equalsIgnoreCase("ThinCrust") || crustRequired.equalsIgnoreCase("CheeseBurst") || crustRequired.equalsIgnoreCase("ClassicPan")){
+            
+            priceOfCrust = crustInPizza( crustRequired , priceOfCrust );
+            }
+        }//trying for reinput
+        }//checking with menu with user data
+        
+        else{
+            System.out.print(" Sorry, Your type of crust is not available. So toppings choice will be skipped  ");
+           
+        }
+        
+
         System.out.println("  ");
         System.out.println("  ");
+        if( crustRequired.equalsIgnoreCase("ThinCrust") || crustRequired.equalsIgnoreCase("CheeseBurst") || crustRequired.equalsIgnoreCase("ClassicPan")){
+
         System.out.println("Please enter type of Pizza toppings required from the list: ");
         System.out.println("  ");
 
-        System.out.println("1. Corn( Rs 105)");
-        System.out.println("2. Chicken( Rs 200)");
-        System.out.println("3. Paneer( Rs 135)");
-        System.out.println("4. Pineapple( Rs 125)");
-        System.out.println("5. Pepperoni( Rs 175)");
+        System.out.println("1. Corn      ( Rs 105)");
+        System.out.println("2. Chicken   ( Rs 200)");
+        System.out.println("3. Paneer    ( Rs 135)");
+        System.out.println("4. Pineapple ( Rs 125)");
+        System.out.println("5. Pepperoni ( Rs 175)");
         System.out.println("  ");
         System.out.print("Your choice: ");
         
         String toppingsRequired = input.next();
+        if( toppingsRequired.equalsIgnoreCase("Corn") || toppingsRequired.equalsIgnoreCase("Chicken") || toppingsRequired.equalsIgnoreCase("Paneer") || toppingsRequired.equalsIgnoreCase("Pineapple") || toppingsRequired.equalsIgnoreCase("Pepperoni")){
         priceOftoppings = toppingsInPizza( toppingsRequired , priceOftoppings );
+        }//checking for same menu input
+
+        else if( !toppingsRequired.equalsIgnoreCase("Corn") || !toppingsRequired.equalsIgnoreCase("Chicken") || !toppingsRequired.equalsIgnoreCase("Paneer") || !toppingsRequired.equalsIgnoreCase("Pineapple") || !toppingsRequired.equalsIgnoreCase("Pepperoni")){
+        System.out.print("Your input is not matching with our menu. Please try one more time: ");
+        toppingsRequired = input.next();
+        {
+        if( toppingsRequired.equalsIgnoreCase("Corn") || toppingsRequired.equalsIgnoreCase("Chicken") || toppingsRequired.equalsIgnoreCase("Paneer") || toppingsRequired.equalsIgnoreCase("Pineapple") || toppingsRequired.equalsIgnoreCase("Pepperoni")){
+        priceOftoppings = toppingsInPizza( toppingsRequired , priceOftoppings );
+        }//trying helping user to reinput
+        }//trying helping user to reinput
+        }//rechecking for if user has input wrong data
+        else{
+            System.out.print(" Sorry, Your type of toppings is not available.");
+
+        }
+    }//Check for crust is available then move to topping
+
+
+    
         System.out.println("  ");
         System.out.println("  ");
 
         System.out.print("Please enter type of Pizza sides required: ");
         System.out.println("  ");
 
-        System.out.println("1. Coke( Rs 45)");
-        System.out.println("2. Fries( Rs 85)");
-        System.out.println("3. Wedges( Rs 110)");
-        System.out.println("4. Nachos( Rs 150)");
+        System.out.println("1. Coke   ( Rs 45)");
+        System.out.println("2. Fries  ( Rs 85)");
+        System.out.println("3. Wedges ( Rs 110)");
+        System.out.println("4. Nachos ( Rs 150)");
         System.out.println("  ");
         System.out.print("Your choice: ");
 
         String sidesRequired = input.next();
-        priceOfsides = sidesInPizza( sidesRequired , priceOfsides );
+        if( sidesRequired.equalsIgnoreCase("Coke") || sidesRequired.equalsIgnoreCase("Fries") || sidesRequired.equalsIgnoreCase("Wedges") || sidesRequired.equalsIgnoreCase("Nachos")){
 
+        priceOfsides = sidesInPizza( sidesRequired , priceOfsides );
+        }//checking menu
+
+        else if( !sidesRequired.equalsIgnoreCase("Coke") || !sidesRequired.equalsIgnoreCase("Fries") || !sidesRequired.equalsIgnoreCase("Wedges") || !sidesRequired.equalsIgnoreCase("Nachos")){
+            System.out.print("Your input is not matching with our menu. Please try one more time: ");
+            sidesRequired = input.next();
+            {
+                if( sidesRequired.equalsIgnoreCase("Coke") || sidesRequired.equalsIgnoreCase("Fries") || sidesRequired.equalsIgnoreCase("Wedges") || sidesRequired.equalsIgnoreCase("Nachos")){
+
+                priceOfsides = sidesInPizza( sidesRequired , priceOfsides );
+
+                }
+            }//giving one more chance
+        }//rechecking same menu input by useer
+        else{
+            System.out.print(" Sorry, Your type of Sides is not available.");
+
+        }
         
+
+
+        count++;
+        System.out.println("  ");
+        System.out.println("  ");
+
+    }while(count < ans );
         int priceOfPizza = 0;
         priceOfPizza = priceOfCrust + priceOftoppings + priceOfsides;
         System.out.println("  ");
         System.out.println("  ");
-
+        System.out.println("Before Discount price: Rs " +priceOfPizza);
+        System.out.println("  ");
+        System.out.println("  ");
 
     
         System.out.print("Please select discount offer 20 percent or 50 percent: ");
         int discountPizza = input.nextInt();
+        if(discountPizza == 20 || discountPizza == 50){
         finalPrizeOfPizza = discountInPizza( discountPizza , priceOfPizza );
-    
+        }
+        else
+        {
+            System.out.print("Sorry.. No discount available in this percent. No discount applied ");
+            finalPrizeOfPizza = priceOfPizza;
+        }
         
+        input.close();
+
+       
+        
+        System.out.println("  ");
+        System.out.println("  ");
+        System.out.println("  ");
         System.out.println("  ");
         System.out.print("Your total bill amount is: Rs " +finalPrizeOfPizza);
         System.out.println("  ");
@@ -162,7 +254,6 @@ public class Buypizza{
         System.out.println("Enjoy The Pizza....Thank You  ");
 
 
-        input.close();
 
 
     }
